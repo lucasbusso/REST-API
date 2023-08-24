a2efb5fca9819b2882001cd3a7299b198aa3dda7
Lucas Busso <lucasxbusso@gmail.com>
2023-08-24 19:02:44 -0300

    error handler

--- src/controllers/item.controller.ts
+++ src/controllers/item.controller.ts
@@ -1,28 +1,39 @@
 import { Request, Response } from "express";
+import { handleHttp } from "../utils/error.handle";
 
 const getItem = (req: Request, res: Response) => {
   try {
-  } catch (error) {}
+  } catch (error) {
+    handleHttp(res, "ERROR_GET_ITEM");
+  }
 };
 
 const getItems = (req: Request, res: Response) => {
   try {
-  } catch (error) {}
+  } catch (error) {
+    handleHttp(res, "ERROR_GET_ITEMS");
+  }
 };
 
 const updateItem = (req: Request, res: Response) => {
   try {
-  } catch (error) {}
+  } catch (error) {
+    handleHttp(res, "ERROR_UPDATE_ITEM");
+  }
 };
 
 const postItem = (req: Request, res: Response) => {
   try {
-  } catch (error) {}
+  } catch (error) {
+    handleHttp(res, "ERROR_POST_ITEM");
+  }
 };
 
 const deleteItem = (req: Request, res: Response) => {
   try {
-  } catch (error) {}
+  } catch (error) {
+    handleHttp(res, "ERROR_DELETE_ITEM");
+  }
 };
 
 export { getItem, getItems, updateItem, postItem, deleteItem };
