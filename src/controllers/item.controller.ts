dfe5e03897d40464a726b6117e3f94d63663719a
Lucas Busso <lucasxbusso@gmail.com>
2023-08-24 18:54:11 -0300

    item controller

---
+++ src/controllers/item.controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+
+const getItem = (req: Request, res: Response) => {
+  try {
+  } catch (error) {}
+};
+
+const getItems = (req: Request, res: Response) => {
+  try {
+  } catch (error) {}
+};
+
+const updateItem = (req: Request, res: Response) => {
+  try {
+  } catch (error) {}
+};
+
+const postItem = (req: Request, res: Response) => {
+  try {
+  } catch (error) {}
+};
+
+const deleteItem = (req: Request, res: Response) => {
+  try {
+  } catch (error) {}
+};
+
+export { getItem, getItems, updateItem, postItem, deleteItem };
