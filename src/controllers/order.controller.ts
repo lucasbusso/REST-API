fa4a1333f34939c138b1a5f090994c017197adfe
Lucas Busso <lucasxbusso@gmail.com>
2023-08-28 14:31:51 -0300

    order controller

---
+++ src/controllers/order.controller.ts
@@ -0,0 +1,16 @@
+import { Response } from "express";
+import { RequestExt } from "../interfaces//reqExt.interface";
+import { handleHttp } from "../utils/error.handle";
+
+const getItems = (req: RequestExt, res: Response) => {
+  try {
+    res.send({
+      data: "ESTO SOLO LO VE LAS PERSONS CON SESSION / JWT",
+      user: req.user,
+    });
+  } catch (e) {
+    handleHttp(res, "ERROR_GET_BLOGS");
+  }
+};
+
+export { getItems };
